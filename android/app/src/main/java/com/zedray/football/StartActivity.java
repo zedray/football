feaafab830d473104a2320fbba3092a9e1c2bb81
Mark Brady <mbrady@yelp.com>
2014-10-03 11:33:40 -0700

    Adding working button UI in Android

--- android/app/src/main/java/com/zedray/football/StartActivity.java
+++ android/app/src/main/java/com/zedray/football/StartActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Message;
 import android.util.Log;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +18,7 @@ import com.getpebble.android.kit.util.PebbleDictionary;
 import java.util.UUID;
 
 
-public class StartActivity extends Activity implements View.OnClickListener {
+public class StartActivity extends Activity implements View.OnTouchListener {
 
     private final static String TAG = "Football";
     private final static UUID PEBBLE_APP_UUID = UUID.fromString("7738b4bb-82a0-40f0-a0e1-a71c2017a98f");
@@ -26,10 +27,8 @@ public class StartActivity extends Activity implements View.OnClickListener {
         STATE,
     }
 
-    private Button mLeft;
-    private Button mSelect;
-    private Button mRight;
-    private int mState = 0;
+    private Button mALeft, mASelect, mARight, mBLeft, mBSelect, mBRight;
+    private int mState = 4;
 
     private final Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -42,12 +41,15 @@ public class StartActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        mLeft = (Button) findViewById(R.id.left);
-        mSelect = (Button) findViewById(R.id.select);
-        mRight = (Button) findViewById(R.id.right);
-        mLeft.setOnClickListener(this);
-        mSelect.setOnClickListener(this);
-        mRight.setOnClickListener(this);
+        mALeft = (Button) findViewById(R.id.a_left);
+        mASelect = (Button) findViewById(R.id.a_select);
+        mARight = (Button) findViewById(R.id.a_right);
+        mALeft.setOnTouchListener(this);
+        mASelect.setOnTouchListener(this);
+        mARight.setOnTouchListener(this);
+        mBLeft = (Button) findViewById(R.id.b_left);
+        mBSelect = (Button) findViewById(R.id.b_select);
+        mBRight = (Button) findViewById(R.id.b_right);
 
         // Listen for Pebble connections.
         PebbleKit.registerPebbleConnectedReceiver(getApplicationContext(), new BroadcastReceiver() {
@@ -84,31 +86,71 @@ public class StartActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.left:
-                Log.w(TAG, "StartActivity.onClick() Left selected");
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction()) {
+            case 0:
+                switch (view.getId()) {
+                    case R.id.a_left:
+                        Log.w(TAG, "StartActivity.onTouch() Left DOWN");
+                        mALeft.setBackgroundResource(R.color.red);
+                        break;
+                    case R.id.a_select:
+                        Log.w(TAG, "StartActivity.onTouch() Select DOWN");
+                        mASelect.setBackgroundResource(R.color.red);
+                        break;
+                    case R.id.a_right:
+                        Log.w(TAG, "StartActivity.onTouch() Right DOWN");
+                        mARight.setBackgroundResource(R.color.red);
+                        break;
+                    default:
+                        break;
+                }
                 break;
-            case R.id.select:
-                Log.w(TAG, "StartActivity.onClick() Select selected");
-                break;
-            case R.id.right:
-                Log.w(TAG, "StartActivity.onClick() Right selected");
-                break;
-            default:
+
+            case 1:
+                switch (view.getId()) {
+                    case R.id.a_left:
+                        Log.w(TAG, "StartActivity.onTouch() Left UP");
+                        mALeft.setBackgroundResource(R.color.green);
+                        break;
+                    case R.id.a_select:
+                        Log.w(TAG, "StartActivity.onTouch() Select UP");
+                        mASelect.setBackgroundResource(R.color.green);
+                        break;
+                    case R.id.a_right:
+                        Log.w(TAG, "StartActivity.onTouch() Right UP");
+                        mARight.setBackgroundResource(R.color.green);
+                        break;
+                    default:
+                        break;
+                }
                 break;
         }
+        return true;
     }
 
     private void updateUi() {
         if ((mState & 0) != 0) {
             Log.w(TAG, "StartActivity.updateUi() 0 selected");
+            mBLeft.setBackgroundResource(R.color.blue);
+        } else {
+            Log.w(TAG, "StartActivity.updateUi() 0 not selected");
+            mBLeft.setBackgroundResource(R.color.green);
         }
         if ((mState & 2) != 0) {
             Log.w(TAG, "StartActivity.updateUi() 2 selected");
+            mBSelect.setBackgroundResource(R.color.blue);
+        } else {
+            Log.w(TAG, "StartActivity.updateUi() 2 not selected");
+            mBSelect.setBackgroundResource(R.color.green);
         }
         if ((mState & 4) != 0) {
             Log.w(TAG, "StartActivity.updateUi() 4 selected");
+            mBRight.setBackgroundResource(R.color.blue);
+        } else {
+            Log.w(TAG, "StartActivity.updateUi() 4 not selected");
+            mBRight.setBackgroundResource(R.color.green);
         }
     }
 }
